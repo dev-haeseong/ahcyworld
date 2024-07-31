bf99d960708903bad3626354238eddb357dd5a1a
haeseong <shs748630@gmail.com>
2024-07-31 10:26:30 +0900

    git setting

---
+++ src/main/java/com/lec/spring/AhcyworldApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhcyworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhcyworldApplication.class, args);
+    }
+
+}
